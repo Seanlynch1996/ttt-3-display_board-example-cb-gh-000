5ff2888a0ee332339ddd93fcac2675711a34b10e
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-11-22 19:51:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  i = 1
-  9.times {
-
-    puts "#{i}"
-
-    i = i + 1
-  }
+  puts "   "
+  puts "|"
+  puts "   "
+  puts "|"
+  puts "   "
+  puts "|"
+  puts "   "
+  puts "|"
