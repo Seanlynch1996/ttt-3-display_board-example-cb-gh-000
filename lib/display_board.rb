e395babf05d1c703f1c2e1c3c1ed12eefe082abd
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-11-22 19:49:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  i = 0
+  i = 1
   9.times {
     if ((i mod 3) == 1) {
       puts "#{i}"
     }
+    i = i + 1
   }
